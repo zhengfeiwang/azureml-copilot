ec2a93711f1f4759b551121464196d2a1f867738
Zhengfei Wang <wangzhengfei.chn@outlook.com>
2023-03-02 16:39:45 +0800

    add acl

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     python_requires="<4.0,>=3.8",
     entry_points={
         "console_scripts": [
-            "azureml-copilot=azureml_copilot._cli.azureml_copilot_commands:main"
+            "azureml-copilot=azureml_copilot._cli.azureml_copilot_commands:main",
+            "acl=azureml_copilot._cli.commands:list_workspaces",
         ]
     },
 )
