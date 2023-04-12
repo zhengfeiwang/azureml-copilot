ffb77806424c6b8eb5386f194ef458d54d4abb1f
Zhengfei Wang <wangzhengfei.chn@outlook.com>
2023-04-12 14:30:32 +0800

    add black util script

---
+++ scripts/black_azure_sdk.ps1
@@ -0,0 +1,12 @@
+# may make conda environment name a variable
+Write-Host "switch to v2-github conda environment..."
+conda activate v2-github
+
+Write-Host "cd to azure-ai-ml directory..."
+$cwd = Get-Location
+Set-Location ~/source/repos/azure-sdk-for-python/sdk/ml/azure-ai-ml
+
+Write-Host "execute black command..."
+black --config ../../../eng/black-pyproject.toml .
+
+Set-Location $cwd  # cd back
